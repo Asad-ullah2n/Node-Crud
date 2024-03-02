00f91b956453d5f3ea99b48ae50ba826ce1fa17d
Asad-ullah2n <asadsunny2n@gmail.com>
2024-03-02 15:52:09 +0500

    instraction

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Product = require("./models/products.model");
+
+app.use(express.json());
 
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
+app.get("/api/products:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const product = await Product.find({});
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 app.listen(3000, () => {
   console.log("Node is runnig on port 3000");
 });
